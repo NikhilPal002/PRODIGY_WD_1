6e8e28a3bf2b67cee71cc9f274bfce80a8da83af
Nikhil <nikhilpal2017@gmail.com>
2024-01-09 17:16:23 +0530

    Written code for getting current location weather

--- src/currentLocation.js
+++ src/currentLocation.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 class weather extends React.Component {
 
+  componentDidMount() {
+    if(navigator.geolocation){
+      this.getPosition()
+      .then((position) => {
+        this.getWeather(position.coords.latitude, position.coords.longitude);
+      })
+      .catch((error) => {
+        this.getweather(28.67, 77.22);
+        alert("You have disabled location service.");
+      });
+    }
+    else{
+      alert("Geolocation is not available");
+    }
+  }
+
   render() {
     return (
       <div>
