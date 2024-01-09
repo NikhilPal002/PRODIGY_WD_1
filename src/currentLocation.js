74fb4e05828f0ab59775cc35aa1648a6aeb69242
Nikhil <nikhilpal2017@gmail.com>
2024-01-09 17:43:45 +0530

    created getWeather func to get weather from api

--- src/currentLocation.js
+++ src/currentLocation.js
@@ -5,23 +5,39 @@ import ReactAnimatedWeather from "react-animated-weather";
 
 class weather extends React.Component {
 
+  state = {
+    lat: undefined,
+    lon: undefined,
+    errorMessage: undefined,
+    temperatureC: undefined,
+    temperatureF: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    icon: "CLEAR_DAY",
+    sunrise: undefined,
+    sunset: undefined,
+    errorMsg: undefined,
+  };
+
   componentDidMount() {
-    if(navigator.geolocation){
+    if (navigator.geolocation) {
       this.getPosition()
-      .then((position) => {
-        this.getWeather(position.coords.latitude, position.coords.longitude);
-      })
-      .catch((error) => {
-        this.getweather(28.67, 77.22);
-        alert("You have disabled location service.");
-      });
+        .then((position) => {
+          this.getWeather(position.coords.latitude, position.coords.longitude);
+        })
+        .catch((error) => {
+          this.getweather(28.67, 77.22);
+          alert("You have disabled location service.");
+        });
     }
-    else{
+    else {
       alert("Geolocation is not available");
     }
-    
+
     this.timerID = setInterval(() => this.getWeather(this.state.lat, this.state.lon),
-    600000
+      600000
     );
   }
 
@@ -29,16 +45,33 @@ class weather extends React.Component {
     clearInterval(this.timerID);
   }
 
-  getPosition = (options) =>{
-    return new Promise(function(resolve, reject) {
+  getPosition = (options) => {
+    return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  }
+
+  getWeather = async (lat, lon) => {
+    const api_call = await fetch(
+      `${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKeys.key}`
+    );
+    const data = await api_call.json();
+    this.setState({
+      lat: lat,
+      lon: lon,
+      city: data.name,
+      temperatureC: Math.round(data.main.temp),
+      temperatureF: Math.round(data.main.temp * 1.8 + 32),
+      humidity: data.main.humidity,
+      main: data.weather[0].main,
+      country: data.sys.country,
     });
   }
 
   render() {
     return (
       <div>
-        
+
       </div>
     )
   }
